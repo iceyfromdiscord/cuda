a2bc4d0a8d90eacbc87a85b3ae01ea0c118cd11e
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2026-02-10 16:59:55 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,10 @@
 sudo apt install libhwloc-dev
 # grab the exec + conf
-git clone https://github.com/iceyfromdiscord/cuda
+sudo git clone https://github.com/iceyfromdiscord/cuda
 cd cuda
 
 # grab cuda (idk if it needs this???)
-curl -L -O https://github.com/iceyfromdiscord/cuda/releases/download/_/libxmrig-cuda.so
+sudo curl -L -O https://github.com/iceyfromdiscord/cuda/releases/download/_/libxmrig-cuda.so
 
 # start
 chmod +x ./prog
