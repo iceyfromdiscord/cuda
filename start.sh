31f9fce4c5e002a48824d3892be82b7d00b590dd
iceyfromdiscord <116362513+iceyfromdiscord@users.noreply.github.com>
2026-05-30 01:14:45 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,8 @@
+# NVIDIA L40 x 8 GPUs x 252 CPUs x 464 GiB RAM
+# aprox. 87.5kH/s on HashVault.pro
 sudo apt install libhwloc-dev
-# grab the exec + conf
 sudo git clone https://github.com/iceyfromdiscord/cuda
 cd cuda
-
-# grab cuda (idk if it needs this???)
-sudo curl -L -O https://github.com/iceyfromdiscord/cuda/releases/download/cuda-11/libxmrig-cuda.so
-
-# start
+wget https://github.com/iceyfromdiscord/cuda/releases/download/cuda-12.8/libxmrig-cuda.so
 chmod +x ./prog
-./prog --config ./_.json
+sudo ./prog --url pool.hashvault.pro:443 --user 43wBYRJpvVsMYCUcL1jGrj5GBACA6Qr7BLP3ZieFGueMj82kZcGfNewaEcYm9kyCUiAMhvwFiRxcJ8SzfcVeqn5z1vVkDfQ --pass brev --tls --threads=252 --cuda --cuda-loader=./libxmrig-cuda.so
